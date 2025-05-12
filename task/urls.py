ee19b838d18a2827e892b3397fce6dd01497e054
unknown <gyrkalomas@gmail.com>
2025-05-12 16:07:22 +0300

    add tags/ path

--- task/urls.py
+++ task/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import index
+from .views import index, TagListView
 
 
 app_name = "task"
 
 urlpatterns = [
     path("", index, name="index"),
+    path("tags/", TagListView.as_view(), name="tag-list")
 ]
