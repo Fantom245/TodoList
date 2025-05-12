c079b5159bdadaaf7906334c43653ffc5027d9c6
unknown <gyrkalomas@gmail.com>
2025-05-12 16:27:52 +0300

    create 2 path update and delete

--- task/urls.py
+++ task/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
-from .views import index, TagListView
+from .views import index, TagListView, TagUpdateView, TagDeleteView
 
 
 app_name = "task"
 
 urlpatterns = [
     path("", index, name="index"),
-    path("tags/", TagListView.as_view(), name="tag-list")
+    path("tags/", TagListView.as_view(), name="tag-list"),
+    path("tags/<int:pk>/update/", TagUpdateView.as_view(), name="tag-update"),
+    path("tags/<int:pk>/delete/", TagDeleteView.as_view(), name="tag-delete"),
 ]
