1c79a7b8f094841b122ce9d7cdf7d05214164340
unknown <gyrkalomas@gmail.com>
2025-05-12 16:30:01 +0300

    create view for update and delete tags

--- task/views.py
+++ task/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
 from django.views import generic
 
 from .models import Task, Tag
@@ -20,3 +21,14 @@ class TagListView(generic.ListView):
     model = Tag
     template_name = "tag_list.html"
     context_object_name = "tag_list"
+
+
+class TagUpdateView(generic.UpdateView):
+    model = Tag
+    fields = ("name",)
+    success_url = reverse_lazy("task:tag-list")
+
+
+class TagDeleteView(generic.DeleteView):
+    model = Tag
+    success_url = reverse_lazy("task:tag-list")
