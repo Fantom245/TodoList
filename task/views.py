5e1d1a795525e1f42f9bf903ed63692889680fb3
unknown <gyrkalomas@gmail.com>
2025-05-12 17:43:21 +0300

    add new views for task and for is_done changes for task

--- task/views.py
+++ task/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, redirect, render
 from django.urls import reverse_lazy
 from django.views import generic
 
@@ -17,10 +17,23 @@ def index(request):
     return render(request, "task/index.html", context=context)
 
 
+def toggle_task_done(request, pk):
+    task = get_object_or_404(Task, pk=pk)
+    task.is_done = not task.is_done
+    task.save()
+    return redirect("task:index")
+
+
 class TagListView(generic.ListView):
     model = Tag
     template_name = "tag_list.html"
     context_object_name = "tag_list"
+
+
+class TagCreateView(generic.CreateView):
+    model = Tag
+    fields = ("name",)
+    success_url = reverse_lazy("task:tag-list")
 
 
 class TagUpdateView(generic.UpdateView):
@@ -32,3 +45,26 @@ class TagUpdateView(generic.UpdateView):
 class TagDeleteView(generic.DeleteView):
     model = Tag
     success_url = reverse_lazy("task:tag-list")
+
+
+class TaskListView(generic.ListView):
+    model = Task
+    template_name = "task_list.html"
+    context_object_name = "task_list"
+
+
+class TaskCreateView(generic.CreateView):
+    model = Task
+    fields = ("content", "deadline", "tags")
+    success_url = reverse_lazy("task:index")
+
+
+class TaskUpdateView(generic.UpdateView):
+    model = Task
+    fields = ("content", "deadline", "tags")
+    success_url = reverse_lazy("task:index")
+
+
+class TaskDeleteView(generic.DeleteView):
+    model = Task
+    success_url = reverse_lazy("task:index")
