22375063e4d75d67ac6e597b34adf00f5ff50ad1
unknown <gyrkalomas@gmail.com>
2025-05-12 15:32:57 +0300

    create view for index

--- task/views.py
+++ task/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render
 
+from .models import Task, Tag
+
 
 def index(request):
-    pass
+    task = Task.objects.all()
+    tag = Tag.objects.all()
+
+    context = {
+        "task": task,
+        "tag": tag,
+    }
+
+    return render(request, "task/index.html", context=context)
