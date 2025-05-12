422aa9a6106b0ceac05f32630d010705f32856e1
unknown <gyrkalomas@gmail.com>
2025-05-12 16:07:59 +0300

    create TagListView

--- task/views.py
+++ task/views.py
@@ -1,15 +1,22 @@
 from django.shortcuts import render
+from django.views import generic
 
 from .models import Task, Tag
 
 
 def index(request):
-    task = Task.objects.all()
-    tag = Tag.objects.all()
+    tasks = Task.objects.all()
+    tags = Tag.objects.all()
 
     context = {
-        "task": task,
-        "tag": tag,
+        "tasks": tasks,
+        "tags": tags,
     }
 
     return render(request, "task/index.html", context=context)
+
+
+class TagListView(generic.ListView):
+    model = Tag
+    template_name = "tag_list.html"
+    context_object_name = "tag_list"
